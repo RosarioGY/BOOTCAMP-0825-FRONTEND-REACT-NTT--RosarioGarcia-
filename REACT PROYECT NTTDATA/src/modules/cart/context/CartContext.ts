ef27886a20f3d27cead3b64e767bdc0fbddc6a62
RosarioGY <rgarciya@emeal.nttdata.com>
2025-09-15 14:43:02 -0500

    Create CartContext.ts

---
+++ REACT PROYECT NTTDATA/src/modules/cart/context/CartContext.ts
@@ -0,0 +1,19 @@
+import { createContext } from 'react';
+import type { CartItem } from '../types/cart.types';
+
+type AddOneResult = 'ok' | 'out_of_stock';
+
+export type CartCtx = {
+  items: CartItem[];
+  totalUnique: number;  
+  totalQty: number;   
+  totalPrice: number;
+  addOne: (p: Omit<CartItem, 'qty'>) => AddOneResult;
+  inc: (id: number) => AddOneResult;
+  dec: (id: number) => void;
+  remove: (id: number) => void;
+  clear: () => void;
+  qtyOf: (id: number) => number;
+};
+
+export const CartContext = createContext<CartCtx | undefined>(undefined);
